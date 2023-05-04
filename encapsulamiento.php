9fb762e850371068233a3a95741485a58e0b190d
Nozen <ramirezedelvalle@gmail.com>
2023-05-04 14:16:32 -0600

    modificacion

--- encapsulamiento.php
+++ encapsulamiento.php
@@ -1,48 +1,42 @@
 <?php
-
 class Producto {
     #DEFINICION DE LOS ATRIBUTOS
-protected string $noMbre;
-public int $precio;
-public bool $disponible;
+    protected string $nombre;
+    public int $precio;
+    public bool $disponible;
 
-#metodo constructor, se ejecuta al efectuar la instancia
+    #metodo constructor, se ejecuta al efectuar la instancia
+    public function __construct(string $nombre, int $precio, bool $disponible)
+    {
+        #SE LES DA VALORES A LOS ATRIBUTOS CON LOS PARAMETROS QUE RECIBE EL MÉTODO CONSTRUCTOR
+        $this->nombre = $nombre;
+        $this->precio = $precio;
+        $this->disponible = $disponible;
+    }
 
-public function __construct(string $nombre, int $precio, bool $disponible)
+    #DEFINICION DE METODOS
+    public function mostrarProducto(){
+        echo "El Producto es: " . $this->nombre . " y su precio es de: " . $this->precio;
+    }
 
-{
-    #SE LES DA VALORS A LOS ATRIBUTOS CON LOS PARAMETROS QUE RECIBE EL MÉTODO CONSTRUCTOR
-    $this->nombre = $nombre;
-    $this->precio = $precio;
-    $this->disponible = $disponible;
-}
-#DEFINICION DE METODOS
-public function mostrarProducto(){
-    echo "El Producto es: " . $this->nombre . " y su precio es de: " . $this->precio;
-}
-
-#METODO GETTER PARA LA PROPIEDAD PROTEGIDA DE NOMBRE
-public function getNombre() : string {
-return $this->nombre;
-}
+    #METODO GETTER PARA LA PROPIEDAD PROTEGIDA DE NOMBRE
+    public function getNombre() : string {
+        return $this->nombre;
+    }
 
     #METODO SETTER PARA LA PROPIEDAD PROTEGIDA DE NOMBRE
     public function setNombre(string $nombre) {
-    $this->nombre = $nombre;
-
+        $this->nombre = $nombre;
     }
 }
 
 $producto = new Producto('Tablet', 200, true);
 //$producto->mostrarProducto(
-echo $producto->nombre; 
-//PRODUCE UN ERROR
 echo $producto->getNombre(); 
-//0BTIENE EL NOMBRE
+//OBTIENE EL NOMBRE
 $producto->setNombre('Nuevo Nombre'); 
 //SETEA EL NOMBRE
 echo $producto->getNombre();
 //OBTIENE EL NOMBRE
-
 ?>
 
